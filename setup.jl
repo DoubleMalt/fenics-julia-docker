b5d97133dccb474f24c33b127ddad1a0ce887c43
Yiannis Simillides <yiannis.simillides.14@ucl.ac.uk>
2018-08-19 17:14:12 +0100

    Update setup.jl

--- setup.jl
+++ setup.jl
@@ -1,6 +1,6 @@
+using Pkg
 Pkg.clone("https://github.com/JuliaPy/PyCall.jl")
 ENV["PYTHON"] = "/usr/bin/python3"
 Pkg.build("PyCall")
 Pkg.add("PyPlot")
 Pkg.add("ProgressMeter")
-Pkg.add("Suppressor")
